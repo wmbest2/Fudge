6f7e8d3824d3c998cfc1414cb2a8d19224a29f50
wmb001 <wmb001@80dbfefb-8c69-48b5-a6b0-056a8968c603>
2008-10-10 13:26:51 +0000

    git-svn-id: http://cscdev.lvc.edu/svn/c331.2007/wmb001/CSC500@3352 80dbfefb-8c69-48b5-a6b0-056a8968c603

--- Parse Tester/main.cpp
+++ Parse Tester/main.cpp
@@ -8,9 +8,17 @@
 #include <iostream>
 #include <lexer.h>
 #include <parser.h>
+#include <dbg.h>
 
 int main()
 {
+	dbg::enable(dbg::all, "cppclass", false);
+	dbg::attach_ostream(dbg::all, "cppclass", std::cout);
+
+	dbg::enable(dbg::all, "parser_helpers", false);
+	dbg::attach_ostream(dbg::all, "parser_helpers", std::cout);
+
+
 	std::ifstream* test_stream = new std::ifstream();
 	test_stream->open("testhfile.h");
 
--- Parse Tester/testhfile.h
+++ Parse Tester/testhfile.h
@@ -7,6 +7,20 @@ namespace tokens
 
 	class my_class : public ::AA::BB::CC::DD::EE::FF
 	{
+		const std::string & helloworld(int x, int y);
+
+		std::string hieric(double amp, sometype should_crash);
+	};
+
+	class second_class : public applesauce
+	{
+		const std::string & helloworld(int x, int y);
+
+		int[][][][][] asdlfkjlsd(double amp, sometype*** should_crash);
+	};
+
+	class no_class
+	{
 
 	};
 }

97d5f985c527451d9089fc707db3552b01dc5745
wmb001 <wmb001@80dbfefb-8c69-48b5-a6b0-056a8968c603>
2008-12-10 19:18:15 +0000

    git-svn-id: http://cscdev.lvc.edu/svn/c331.2007/wmb001/CSC500@3395 80dbfefb-8c69-48b5-a6b0-056a8968c603

---
+++ FUDGE/Debug/main.d
@@ -0,0 +1,48 @@
+main.d main.o: ../main.cpp /home/wmb001/workspace/CSC500/Parser/parser.h \
+  /home/wmb001/workspace/CSC500/GenHelpers/cppvar.h \
+  /home/wmb001/workspace/CSC500/GenHelpers/cpptype.h \
+  /home/wmb001/include/dbg.h \
+  /home/wmb001/workspace/CSC500/GenHelpers/stackable.h \
+  /home/wmb001/workspace/CSC500/GenHelpers/cppclass.h \
+  /home/wmb001/workspace/CSC500/GenHelpers/memfunc.h \
+  /home/wmb001/workspace/CSC500/GenHelpers/cppvar.h \
+  /home/wmb001/workspace/CSC500/GenHelpers/memfuncstr.h \
+  /home/wmb001/workspace/CSC500/GenHelpers/cppnamespace.h \
+  /home/wmb001/workspace/CSC500/GenHelpers/memfunc.h \
+  /home/wmb001/workspace/CSC500/GenHelpers/cppnamespace.h \
+  /home/wmb001/workspace/CSC500/CppParlexser/plexser.h \
+  /home/wmb001/workspace/CSC500/CppParlexser/cpptoken.h \
+  /home/wmb001/workspace/CSC500/Resolver/resolver.h \
+  /home/wmb001/workspace/CSC500/GenHelpers/exceptions.h
+
+/home/wmb001/workspace/CSC500/Parser/parser.h:
+
+/home/wmb001/workspace/CSC500/GenHelpers/cppvar.h:
+
+/home/wmb001/workspace/CSC500/GenHelpers/cpptype.h:
+
+/home/wmb001/include/dbg.h:
+
+/home/wmb001/workspace/CSC500/GenHelpers/stackable.h:
+
+/home/wmb001/workspace/CSC500/GenHelpers/cppclass.h:
+
+/home/wmb001/workspace/CSC500/GenHelpers/memfunc.h:
+
+/home/wmb001/workspace/CSC500/GenHelpers/cppvar.h:
+
+/home/wmb001/workspace/CSC500/GenHelpers/memfuncstr.h:
+
+/home/wmb001/workspace/CSC500/GenHelpers/cppnamespace.h:
+
+/home/wmb001/workspace/CSC500/GenHelpers/memfunc.h:
+
+/home/wmb001/workspace/CSC500/GenHelpers/cppnamespace.h:
+
+/home/wmb001/workspace/CSC500/CppParlexser/plexser.h:
+
+/home/wmb001/workspace/CSC500/CppParlexser/cpptoken.h:
+
+/home/wmb001/workspace/CSC500/Resolver/resolver.h:
+
+/home/wmb001/workspace/CSC500/GenHelpers/exceptions.h:
